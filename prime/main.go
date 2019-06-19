91406413b401b87ae0f05a31c0e00780ffc97e13
lambig <ceolnua51@gmail.com>
2019-06-19 11:53:27 +0900

    素数を求めるプログラムを実装する練習

---
+++ prime/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	var max int
+	var primes []int
+	Individable := func(divident int, divisors []int) bool {
+		for i := 0; i < len(primes) && primes[i] <= divident/2; i++ {
+			if divident%divisors[i] == 0 {
+				return false
+			}
+		}
+		return true
+	}
+	ToStringSlice := func(intSlice []int) []string {
+		stringSlice := make([]string, len(intSlice))
+		for i := 0; i < len(intSlice); i++ {
+			stringSlice[i] = strconv.Itoa(intSlice[i])
+		}
+		return stringSlice
+	}
+
+	fmt.Scan(&max)
+	for i := 2; i <= max; i++ {
+		if Individable(i, primes) {
+			primes = append(primes, i)
+		}
+	}
+	stringPrimes := ToStringSlice(primes)
+
+	fmt.Println(strconv.Itoa(max) + "までの素数は " + strings.Join(stringPrimes, ", "))
+	fmt.Println("個数は " + strconv.Itoa(len(primes)))
+}
